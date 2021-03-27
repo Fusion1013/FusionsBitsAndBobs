7a72dc42393608f03af47c8262c039cb26ac2997
William Reutmer <william.reutmer@gmail.com>
2021-03-27 02:57:08 +0100

    Remove unused imports

--- src/main/java/me/fusion1013/bitsandbobs/commands/UHCCommand.java
+++ src/main/java/me/fusion1013/bitsandbobs/commands/UHCCommand.java
@@ -1,9 +1,7 @@
 package me.fusion1013.bitsandbobs.commands;
 
 import me.fusion1013.bitsandbobs.gui.GUIHolder;
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
--- src/main/java/me/fusion1013/bitsandbobs/gui/TeamsGUI.java
+++ src/main/java/me/fusion1013/bitsandbobs/gui/TeamsGUI.java
@@ -4,7 +4,6 @@ import me.fusion1013.bitsandbobs.util.ItemStackUtil;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
-import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.scoreboard.DisplaySlot;
 import org.bukkit.scoreboard.Objective;
